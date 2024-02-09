149a8b569d7a6d86ba6d5f66c9df334b60811930
charles <charles.braquet@e.email>
2024-02-09 22:01:40 +0100

    starting paxScr

---
+++ paxApps/paxScr/README.md
@@ -0,0 +1,29 @@
+genBindings
+===========
+
+
+Presentation
+------------
+
+*genBindings* generates the files for binding the *pax application* (i.e. desiXY-cli and desiXY-ui) to the selected *geometrix design libraries*.
+
+*genBindings* reads the package.json, search for the key *paxApps* and generates the files:
+- the scss file for the *svelte* app *desiXY-ui*
+- the typescrit files *designList.ts* for *desiXY-cli* and *desiXY-ui*
+- modify the *package.json* of *desiXY-cli* and *desiXY-ui* for adding the dependencies to the *geometrix design libraries*
+
+
+Getting started
+---------------
+
+```bash
+git clone https://github.com/charlyoleg2/parame_paxApps
+cd parame_paxApps/paxApps/paxScr
+npm install
+npm ci
+npm run run
+```
+
+
+
+
---
+++ paxApps/paxScr/src/genBindings.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+// genBindings.ts
+
+import fs from 'fs';
+
+interface tPaxAppConfig {
+	colorBg: string;
+	colorTitle: string;
+	libs: string[];
+}
+
+const k_paxApps = 'paxApps';
+
+function getPackageJson(jsonPath: string): tPaxAppConfig {
+	let rObj: tPaxAppConfig = {
+		colorBg: "DarkCyan",
+		colorTitle: "aquamarine",
+		libs: []
+	};
+	if (!jsonPath.endsWith('.json')) {
+		console.log(`err129: ${jsonPath} has an unexpected file extension`);
+		process.exit(1);
+	}
+	if (!fs.existsSync(jsonPath)) {
+		console.log(`err134: file ${jsonPath} doesn't exist`);
+		process.exit(1);
+	}
+	try {
+		const fContentStr = fs.readFileSync(jsonPath, 'utf8');
+		const packageJson = JSON.parse(fContentStr);
+		if (k_paxApps in packageJson) {
+			rObj = packageJson[k_paxApps];
+		} else {
+			console.log(`err142: JSON ${jsonPath} doesn't have the key '${k_paxApps}'`);
+			process.exit(1);
+		}
+	} catch(err) {
+		console.log(`err156: error by parsing ${jsonPath}`);
+		console.log(err);
+		process.exit(1);
+	}
+	return rObj;
+}
+
+console.log('genBindings.ts says hello!');
+const cfg = getPackageJson('../../package.json');
+console.log(cfg);
+console.log('genBindings.ts says bye!');
+

c046301ea5942ddc58d4d08c7086ffd4c426be81
charles <charles.braquet@e.email>
2024-02-10 23:09:10 +0100

    cli use generated designList

--- paxApps/desiXY-cli/src/desiXY-cli.ts
+++ paxApps/desiXY-cli/src/desiXY-cli.ts
@@ -4,12 +4,7 @@
 import type { tAllPageDef } from 'geometrix';
 import { geom_cli } from 'geomcli';
 import packag from '../package.json';
-import { myCircleDef, myRectangleDef } from 'desi01';
-
-const designList: tAllPageDef = {
-	'myGroup1/myCircle': myCircleDef,
-	'myGroup1/myRectangle': myRectangleDef
-};
+import { designList } from './designList';
 
 //console.log('desiXY-cli says hello');
 await geom_cli(process.argv, designList, packag, 'output');
